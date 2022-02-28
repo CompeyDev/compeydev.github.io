ed853a3ac339c735dd00a6e02c8a0cb6e8eca7fc
DevComp <hi@devcomp.tk>
2022-02-28 18:19:30 +0530

    Initialize.

---
+++ resources/worker.js
@@ -0,0 +1,16 @@
+
+
+	var discord_id = "893762371770802227";
+
+    function updateStats() {
+                    $.getJSON("https://api.lanyard.rest/v1/users/" + discord_id, (data) => {
+                        data = data.data;
+                        if (data.listening_to_spotify) {
+                            $("#spotify").html(`<a class="noAStyle" target="_blank" href="https://spotify.devcomp.tk"><b>${data.spotify.song}</b> \n by <i>${data.spotify.artist}</i></a>`);
+                        } else {
+                            $("#spotify").html("Not listening to anything.")
+                        }
+            })
+    }
+    updateStats();
+    setInterval(updateStats, 30000);
